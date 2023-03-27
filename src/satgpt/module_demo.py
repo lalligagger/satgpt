388ff022a8e4729c1a694bc9acac1c1ea672ad34
Kevin Lalli <klalli86@gmail.com>
2023-03-27 13:40:48 -0700

    build cleanup

--- src/satgpt/module_demo.py
+++ src/satgpt/module_demo.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def print_something(something):
+def print_something(something="module test"):
     print(something, flush=True)
     return something
+
+if __name__ == '__main__':
+    print_something()
