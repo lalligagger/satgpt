c698bac98bd385b1fa18eca1705263236f7bf0f5
Kevin Lalli <klalli86@gmail.com>
2023-03-27 15:11:30 -0700

    more setup.py tries, no luck

--- tests/test_module_demo.py
+++ tests/test_module_demo.py
@@ -4,7 +4,7 @@
 import unittest
 
 from satgpt.module_demo import print_something
-# import sgpt works
+import sgpt #works
 
 class TestModuleDemo(unittest.TestCase):
     def test_print_something(self):
