bc890b091d33694b1e3eb5f7f77d0f88282db0c5
Kevin Lalli <klalli86@gmail.com>
2023-04-16 15:36:57 -0700

    new setup test

--- setup.py
+++ setup.py
@@ -8,12 +8,21 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 def add_satgpt_prompt():
+    # read in default shell.json
+    shell_path = getenv("HOME") + "/.config/shell_gpt/roles/shell.json"
+    with open(shell_path, "r") as f:
+        shell_json = json.load(f)
+    
+    # append contents of ./config/alpha.txt the 'role' value of shell_json dict
+    with open("./.config/alpha.txt", "r") as f:
+        shell_json["role"] += f.read()
+
     file_path = getenv("HOME") + "/.config/shell_gpt/roles/satgpt.json"
     # copy from ./.config/satgpt.json to file_path if it doesn't exist already
     if not os.path.exists(file_path):
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "w") as f:
-            json.dump({}, f)
+            json.dump(shell_json, f, indent=4)
 
 # https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
 class PostDevelopCommand(develop):
